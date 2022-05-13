d525d9891fcd552d2b04fd2207296c8094e24896
Sanjeev Kumar <sanjeevkumar@me.com>
2022-05-13 17:32:56 +0530

    Added code to support login based component access for home page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { useQuery } from "@apollo/client";
 
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
+import ThoughtList from "../components/ThoughtList";
+import ThoughtForm from "../components/ThoughtForm";
 
-import { QUERY_THOUGHTS } from '../utils/queries';
-import ImageManager from '../components/ImageManager';
+import { QUERY_THOUGHTS, QUERY_IMAGE } from "../utils/queries";
+import ImageManager from "../components/ImageManager";
+import Auth from "../utils/auth";
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
+  const { loading, data } = useQuery(QUERY_IMAGE);
+  const images = data?.images || [];
+
+  // if (!imageURLList.length) {
+  //   return <h3>Upload your image Files!</h3>;
+  // }
 
   return (
     <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <ImageManager test="test"/>
-        </div>
-        {/* <div
+      <h3>
+        Managing your photoshoot images has never been easy but now possible!
+      </h3>
+
+      {Auth.loggedIn() ? (
+        <div className="flex-row justify-center">
+          <div
+            className="col-12 col-md-10 mb-3 p-3"
+            style={{ border: "1px dotted #1a1a1a" }}
+          >
+            <ImageManager test="test" />
+          </div>
+
+          {/* <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
@@ -36,7 +47,37 @@ const Home = () => {
             />
           )}
         </div> */}
-      </div>
+        </div>
+      ) : (
+        <div>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <div>
+              <h4>Historically Uploaded Images</h4>
+              <div className="flex-row my-4">
+                {images &&
+                  images.map((image) => (
+                    <div key={image._id} className="col-12 mb-3 pb-3">
+                      <div className="p-3 text-light">
+                        <h5 className="card-header bg-dark">
+                          style={image.style.styleCode} colour=
+                          {image.colour.colourCode} filename=
+                          {image.imageName} type={image.imageName.split('-')[2]}{" "}
+                          upload=
+                          {image.imageName.split('-')[3]}
+                        </h5>
+                        <h5 className="card-header">
+                          <img src={image.imageURL} width="100" height="100" />
+                        </h5>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </main>
   );
 };
